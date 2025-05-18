448da90fd248a4e8ff68c45e4c26c39ad55cc445
Ludovic Legros <contact@ludoviclegros.fr>
2025-05-18 20:34:38 +0200

    rajout env gitignore

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class UserType extends AbstractType
 {
@@ -16,18 +17,22 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('password', PasswordType::class, [
-                'label' => 'Mot de passe',
-                'constraints' => [
-                    new NotBlank(['message' => 'Le mot de passe est obligatoire']),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères',
-                    ]),
-                ],
-            ])
-            ->add('passwordConfirm', PasswordType::class, [
-                'label'=>'Confirmez votre mot de passe',
+            // ->add('password', PasswordType::class, [
+            //     'label' => 'Mot de passe',
+            //     'constraints' => [
+            //         new NotBlank(['message' => 'Le mot de passe est obligatoire']),
+            //         new Length([
+            //             'min' => 6,
+            //             'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères',
+            //         ]),
+            //     ],
+            // ])
+            ->add('password', RepeatedType::class, [
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'Les mots de passe doivent correspondre.',
+                    'required' => true,
+                    'first_options'  => ['label' => 'Mot de passe'],
+                    'second_options' => ['label' => 'Confirmer le mot de passe'],
             ])
         ;
     }
